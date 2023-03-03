16626d9add3ce6517e20c5d211e6b94e21a2ace6
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-03-03 16:21:39 +0000

    Added a Tabular Inline

--- store/admin.py
+++ store/admin.py
@@ -14,7 +14,8 @@ class InventoryFilter(admin.SimpleListFilter):
     def lookups(self, request, model_admin):
         return [
             ('<10', 'Low'),
-        ]        
+        ]
+
     def queryset(self, request, queryset):
         if self.value() == '<10':
             return queryset.filter(inventory__lt=10)
@@ -24,12 +25,13 @@ class InventoryFilter(admin.SimpleListFilter):
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
     search_fields = ['title']
-    @admin.display(ordering = 'product_count')
+
+    @admin.display(ordering='product_count')
     def products_count(self, collection):
         url = reverse('admin:store_product_changelist')
         return format_html('<a href="{}?collection__id={}">{}</a>', url, collection.id, collection.product_count)
         # return collection.product_count
-    
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(product_count=Count('product'))
 
@@ -41,12 +43,14 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
-    list_display = ['title', 'unit_price', 'inventory' ,'inventory_status', 'collection']
-    list_editable = ['unit_price','inventory']
+    list_display = ['title', 'unit_price',
+                    'inventory', 'inventory_status', 'collection']
+    list_editable = ['unit_price', 'inventory']
     list_per_page = 10
-    list_filter = ['collection','last_update',InventoryFilter]
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    search_fields = ['title']
 
-    @admin.display(ordering = 'inventory')
+    @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'Low'
@@ -55,26 +59,38 @@ class ProductAdmin(admin.ModelAdmin):
     @admin.action(description='Clear Inventory')
     def clear_inventory(self, request, queryset):
         updated_count = queryset.update(inventory=0)
-        self.message_user(request, f'{updated_count} products were successfully updated.')
+        self.message_user(
+            request, f'{updated_count} products were successfully updated.')
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership','orders']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name','last_name']
+    ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
-    @admin.display(ordering = 'order_count')
+    @admin.display(ordering='order_count')
     def orders(self, customer):
         url = reverse('admin:store_order_changelist')
         return format_html('<a href="{}?customer__id={}">{}</a>', url, customer.id, customer.order_count)
-    
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(order_count=Count('order'))
 
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    model = models.OrderItem
+    min_num = 1
+    max_num = 10
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id','placed_at', 'customer']
+    list_display = ['id', 'placed_at', 'customer']
     ordering = ['id']
     autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
--- store/models.py
+++ store/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True,related_name='+')
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         ordering = ['title']
 
@@ -19,8 +22,9 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField(null=True,blank=True)
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2,validators=[MinValueValidator(1)])
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(1)])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -28,9 +32,10 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         ordering = ['title']
+
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -49,12 +54,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
-    
-    class Meta:            
-        ordering = ['first_name','last_name']
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Order(models.Model):
@@ -65,26 +70,31 @@ class Order(models.Model):
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING, 'Pending'),
         (PAYMENT_STATUS_COMPLETE, 'Complete'),
-        (PAYMENT_STATUS_FAILED, 'Failed'), 
+        (PAYMENT_STATUS_FAILED, 'Failed'),
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     ciyt = models.CharField(max_length=255)
     # zip = models.DecimalField(max_digits=6,decimal_places=1)
-    customer = models.OneToOneField(Customer,on_delete=models.CASCADE,primary_key=True) 
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True)
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
